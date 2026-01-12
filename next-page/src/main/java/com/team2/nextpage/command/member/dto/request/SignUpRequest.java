4ea31347bd5e945b391a7c2c1005704153bc932e
ikth-kim <ikth.kim@gmail.com>
2026-01-12 14:52:47 +0900

    feature : 회원가입 - dto 수정

--- next-page/src/main/java/com/team2/nextpage/command/member/dto/request/SignUpRequest.java
+++ next-page/src/main/java/com/team2/nextpage/command/member/dto/request/SignUpRequest.java
@@ -2,16 +2,28 @@ package com.team2.nextpage.command.member.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 회원가입 요청 DTO
  *
  * @author 김태형
  */
-public record SignUpRequest(
-    @NotBlank(message = "이메일은 필수입니다.") @Size(min = 4, max = 100, message = "이메일은 100자 이하로 입력해주세요.") String userEmail,
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class SignUpRequest {
 
-    @NotBlank(message = "비밀번호는 필수입니다.") String userPw,
+  @NotBlank(message = "이메일은 필수입니다.")
+  @Size(min = 4, max = 100, message = "이메일은 100자 이하로 입력해주세요.")
+  private String userEmail;
 
-    @NotBlank(message = "닉네임은 필수입니다.") String userNicknm) {
+  @NotBlank(message = "비밀번호는 필수입니다.")
+  private String userPw;
+
+  @NotBlank(message = "닉네임은 필수입니다.")
+  private String userNicknm;
 }
